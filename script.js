6d0371c32e66cf0325923c8c5aeda392fe6069ef
Eshan Ahmed <ehsan.ahmed@northsouth.edu>
2022-01-26 14:26:05 +0600

    variables is JS

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log(5)
+console.log(54)
+// console.log(5)
+// console.log(5)
+
+var nameOfAnimal = "tiger";
+var gender = "male";
+
+var genderAndNameOfAnimal = nameOfAnimal+ gender;
+console.log(genderAndNameOfAnimal);
+console.log(typeof (genderAndNameOfAnimal));
+var price = 65.5
+console.log(typeof price)
+console.log(parseInt(price))
+var age = "75.45454";
+console.log(parseFloat(age));
+var ageInFloat = parseFloat(age)
+var ageInInteger = parseInt(age)
+console.log(ageInFloat);
+console.log(ageInInteger);
+console.log(typeof ageInFloat);
+console.log(ageInFloat.toFixed(1));

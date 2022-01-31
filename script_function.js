93ab0c5982e477583b929e688d0efd2e535a956b
Eshan Ahmed <ehsan.ahmed@northsouth.edu>
2022-01-31 15:37:43 +0600

    practice done

---
+++ script_function.js
@@ -0,0 +1,39 @@
+/*
+    function tableOf13(){
+        for(i=1;i<= 10;i++){
+            console.log("13 X " +i +"="+ (13*i) );
+        }
+    }
+
+    tableOf13();
+*/
+
+/*
+function converToLowerCase(word){
+    return word.toLowerCase()
+}
+
+console.log(converToLowerCase("Hello Eshan"))
+*/
+/*
+function concateName(firstName,lastName){
+    return firstName+" "+lastName;
+}
+
+concateName("Eshan","Ahmed")
+console.log(concateName("Eshan","Ahmed"))
+*/
+/*
+function converToSquare(digit){
+    return digit*digit;
+}
+
+console.log(converToSquare(4))*/
+
+const pizza={
+    toppings:["cheese","sauce","pepperoni"],
+    crust: "deep dish",
+    serves: 2,
+}
+console.log(pizza);
+console.log(pizza.toppings[2]);

49dc7367213bec2beae3c6c1dcb0fcd87dcbc1ba
Eshan Ahmed <ehsan.ahmed@northsouth.edu>
2022-01-31 18:04:51 +0600

    More Practice done

--- script_practice_module-20.js
+++ script_practice_module-20.js
@@ -1,4 +1,4 @@
-/*
+
 function isLeapYear(year){
     if(year%4 == 0){
         return true;
@@ -9,8 +9,8 @@ function isLeapYear(year){
 }
 
 console.log(isLeapYear(2023));
-*/
-/*
+
+
 function isAgeOddOrEven(age){
     if(age%2 == 0){
         return true;
@@ -22,11 +22,34 @@ function isAgeOddOrEven(age){
 
 console.log(isAgeOddOrEven(0))
 
-*/
-/*
+
+
 function converHourToMinute(hour){
     return hour*60;
 }
 
 console.log(converHourToMinute(0.5))
-*/
+
+
+function factorial(number){
+    let result = 1;
+    for(i=1;i <= number; i++){
+        result = result* i;
+    }
+    return result;
+}
+
+console.log(factorial(4))
+*
+
+function factorial(number){
+    let result=1;
+    let i=1;
+    while(i <= number){
+        result = result * i;
+        i++;
+    }
+    return result;
+}
+
+console.log(factorial(3));

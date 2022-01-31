2ac2a8a3cb9675c8811f017ff6187b3c4d9c443c
Eshan Ahmed <ehsan.ahmed@northsouth.edu>
2022-01-31 15:50:13 +0600

    module 20 practice done

---
+++ script_practice_module-20.js
@@ -0,0 +1,32 @@
+/*
+function isLeapYear(year){
+    if(year%4 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(isLeapYear(2023));
+*/
+/*
+function isAgeOddOrEven(age){
+    if(age%2 == 0){
+        return true;
+    }
+    else if(age % 2 != 0){
+        return false;
+    }
+}
+
+console.log(isAgeOddOrEven(0))
+
+*/
+/*
+function converHourToMinute(hour){
+    return hour*60;
+}
+
+console.log(converHourToMinute(0.5))
+*/

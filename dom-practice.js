71835e57cce64549fa989c7cc5c66d1b4027568c
Eshan Ahmed <ehsan.ahmed@northsouth.edu>
2022-02-15 21:06:25 +0600

    task 2 done

--- dom-practice.js
+++ dom-practice.js
@@ -17,4 +17,25 @@ listBtn.addEventListener("click",function(event){
     number++;
 })
 
+let inputField = document.getElementById("input-field");
+console.log(inputField);
+let increaseBtn= document.getElementById("submit-btn");
+console.log(increaseBtn);
+let count = inputField.value;
+count = parseInt(count);
+console.log(count);
+
+increaseBtn.addEventListener("click",function(event){
+    event.preventDefault();
+    count++
+    inputField.value = count;
+    if(count > 5){
+        increaseBtn.style.color = "white";
+        increaseBtn.style.backgroundColor = "red";
+        increaseBtn.innerText = "Enough!!";
+        increaseBtn.disabled = true;
+    }
+    
+
+})
 
